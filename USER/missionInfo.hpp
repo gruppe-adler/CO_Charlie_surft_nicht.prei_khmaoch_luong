80f857d010ce7fb424619d1ce898fc7ceb56f9fe
Eras308 <31324871+Eras308@users.noreply.github.com>
2018-07-13 21:32:26 +0200

    first commit

---
+++ USER/missionInfo.hpp
@@ -0,0 +1,10 @@
+/*
+*   Legt allgemeine Information über die Mission fest.
+*/
+
+author = "Ainer für Gruppe Adler";                                               // Missionsersteller
+onLoadName = "Charlie surft nicht";                                                   // Name der Mission
+onLoadMission = "";                                                             // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
+loadScreen = "data\loadpic.jpg";                                                // Ladebild
+overviewPicture = "";                                                           // Bild, das in der Missionsauswahl angezeigt wird
+overviewText = "";                                                              // Text, der in der Missionsauswahl angezeigt wird
